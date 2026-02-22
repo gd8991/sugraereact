bcd57746b9e4a882e60c901b9e6fa9b800196808
gd8991 <92352227+gd8991@users.noreply.github.com>
2026-02-22 21:15:13 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useLayoutEffect } from 'react';
+import { useState, useLayoutEffect } from 'react';
 import Loader from './components/Loader';
 import Header from './components/Header';
 import Hero from './components/Hero';
